069a113a7848a14a1dd415e65d8b3ec26dc810aa
Daehee Kim <rlarl1999@gmail.com>
2025-05-23 10:34:49 +0900

    Update main file

--- hStouffer/hStouffer.py
+++ hStouffer/hStouffer.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
 #=============================================================================#
 # Author    : DaeHee Kim
-# Date      : 2024-03-21
-# Usage     : DESeq2 결과물들 합쳐서 meta analysis
+# Date      : 2025-05-23
+# Usage     : Perform meta-analysis by integrating DESeq2 result files
 # Example   : hStouffer.py -d dataset/low_fat/ -o meta_low_fat
 # Description   : hStouffer is a meta-analysis method that combines p-values from multiple studies to identify differentially expressed genes (DEGs) across datasets.
 #=============================================================================#
